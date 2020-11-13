ae37054c4339cc31c0b686482d8207ee2f9c3a28
ducnt <ducnt444@gmail.com>
2020-11-13 21:21:17 +0700

    rfhfh

---
+++ day11/test.js
@@ -0,0 +1,9 @@
+
+//let name = prompt("Tên bạn là gì", "")
+//let dob = prompt("Ngày sinh của bạn", "");
+//let gender = prompt("Giới tính của bạn", "");
+//let place = prompt("Nơi ở của bạn", "");
+
+//confirm(`Xin hãy xác nhận: Bạn là ${name}, sinh ngày ${dob}, giới tính ${gender}, sống tại ${place}`);
+
+console.log(Boolean(!""));
