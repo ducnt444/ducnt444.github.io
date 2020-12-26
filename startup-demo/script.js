75c57bdfd88375a3e69cb0f452e7d3d7a13dfd42
ducnt <ducnt444@gmail.com>
2020-12-27 00:21:06 +0700

    sv

--- startup-demo/script.js
+++ startup-demo/script.js
@@ -1,7 +1,7 @@
 /*-----------bài 6-----------*/
 let check = 0; //khi check6 == 0 thì menu sẽ ẩn, check6 == 1 thì menu sẽ hiện. check6 thay đổi qua mỗi lần click
 
-let dropdown = function() {
+function dropdown1() {
   let menu = document.getElementById("menu");
   
   if (check == 0) {  //nếu khi click mà check đang == 0
@@ -15,4 +15,21 @@ let dropdown = function() {
   }
 }
 
-document.getElementById("menu-button").onclick = dropdown;
+document.getElementById("menu-button").addEventListener("click", dropdown1);
+
+
+function dropdown2() {
+  let menu = document.getElementById("language-menu");
+  
+  if (check == 0) {  //nếu khi click mà check đang == 0
+    menu.style.visibility = "visible"; 
+    menu.style.opacity  = "1";   
+    check = 1;   //rồi chuyển check về == 1 
+  } else if (check == 1) { //nếu khi click mà check đang == 1
+    menu.style.visibility = "hidden"; 
+    menu.style.opacity  = "0";
+    check = 0;   //rồi chuyển check về == 0  
+  }
+}
+
+document.getElementById("footer-button").addEventListener("click", dropdown2);
