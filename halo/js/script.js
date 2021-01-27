fd7d31e41ac1c3e085a1669d7e286b584470ee27
ducnt <ducnt444@gmail.com>
2021-01-27 21:46:08 +0700

    214

---
+++ halo/js/script.js
@@ -0,0 +1,11 @@
+
+//slick js
+$(document).ready(function(){
+  $(".single-item").slick({
+    arrows: true,
+    prevArrow:
+      "<img class='a-left control-c prev slick-prev' src='https://lh3.googleusercontent.com/proxy/0Hug9AB9PxLyQXkvfmXUaw8h-vTSYI7Nh2y9UNvy011zT3y5NNjitm2LpWTftAkFSBlqmzn7ZG4pZvUD7lH6b4sV0QpylxsKYdw0L8MIWA-que8'>",
+    nextArrow:
+      "<img class='a-right control-c next slick-next' src='../img/misc-img/next-arrow-xs.png'>",
+  });
+});
