df1b03e1a9db845e2d237d3f1e057391ebfd3e0c
ducnt <ducnt444@gmail.com>
2021-02-08 17:36:50 +0700

    eg

--- halo/js/script.js
+++ halo/js/script.js
@@ -23,3 +23,71 @@ function searchBarToggler() {
     searchBarStatus = 0;
   }
 }
+
+let logregStatus = "login";
+let regPart = document.getElementsByClassName("reg");
+let logPart = document.getElementsByClassName("log");
+let logreg = document.getElementById("logreg");
+
+function logregSwitch() {
+  if (logregStatus == "login") {
+    for (let i = 0; i < logPart.length; i++) {
+      logPart[i].style.display = "none";
+    }
+
+    regPart[0].style.display = "block";
+    for (let i = 1; i < 3; i++) {
+      regPart[i].style.display = "flex";
+    }
+    for (let i = 3; i < regPart.length; i++) {
+      regPart[i].style.display = "block";
+    }
+
+    logregStatus = "register";
+  } else {
+    for (let i = 0; i < regPart.length; i++) {
+      regPart[i].style.display = "none";
+    }
+
+    for (let i = 0; i < 4; i++) {
+      logPart[i].style.display = "block";
+    }
+
+    logPart[4].style.display = "flex";
+    logPart[5].style.display = "block";
+
+    logregStatus = "login";
+  }
+}
+    
+
+
+/*     logPart[0].style.visibility = 'hidden !important';
+   
+   for (let i = 1; i < 3; i++) {
+      regPart[i].style.display = "flex !important";
+    }
+    for (let i = 3; i < regPart.length; i++) {
+      regPart[i].style.display = "block !important";
+    }
+    
+    for (let i = 1; i < logPart.length; i++) {
+      logPart[i].style.display = "none !important";
+    } 
+    
+    console.log(logPart[0]);
+    console.log(getComputedStyle(logPart[0]).display);
+
+    logregStatus = "register";
+  } else {
+    for (let i = 1; i < 4; i++) {
+      logPart[i].style.display = "block !important";
+    }
+    logPart[4].style.display = "flex !important";   
+    logPart[5].style.display = "block !important";
+
+    for (let i = 1; i < regPart.length; i++) {
+      regPart[i].style.display = "none !important";
+    }
+    regPart[0].style.display = "none !important";
+    logregStatus = "login"; */
