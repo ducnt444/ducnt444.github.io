9e045cc0224ff508bf11e7a4a8e9bdb7d02488b5
ducnt <ducnt444@gmail.com>
2021-02-09 17:20:26 +0700

    ok

--- halo/js/script.js
+++ halo/js/script.js
@@ -1,3 +1,40 @@
+$(document).ready(function(){
+  $('.hot-row').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
 /* let i = 0;
 
 let counter = setInterval(function(){
