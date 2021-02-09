7dd6309cfe65961ac3ee51c8a839b3a72eea1a9b
ducnt <ducnt444@gmail.com>
2021-02-09 19:10:10 +0700

    eg

--- halo/js/script.js
+++ halo/js/script.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-  $('.hot-row').slick({
-    infinite: true,
+  $('.slider-row').slick({
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     variableWidth: true,
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          infinite: true,
-          dots: true
         }
       },
       {
