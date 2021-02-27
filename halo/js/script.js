23f7af1a2b963fe65feef2358a7d19fe81a9b8f1
ducnt <ducnt444@gmail.com>
2021-02-27 10:57:42 +0700

    rh

--- halo/js/script.js
+++ halo/js/script.js
@@ -36,6 +36,7 @@ $(document).ready(function(){
   });
 });
 
+
 /* --------------- search bar toggler --------------- */
 let searchBarStatus = 0;
 
@@ -52,6 +53,7 @@ function searchBarToggler() {
 }
 
 /* --------------- hết search bar toggler --------------- */
+
 
 
 /* --------------- login/register toggler --------------- */
@@ -94,6 +96,7 @@ function logregSwitch() {
 /* --------------- hết login/register toggler --------------- */
 
 
+
 /* --------------- swap detail image --------------- */
 
 let mainImg = document.getElementsByClassName("details__main-img")[0];
@@ -110,6 +113,7 @@ for (let i = 0; i < subImgs.length; i++) {
 /* --------------- hết swap detail image --------------- */
 
 
+
 /* --------------- .main-info__toggler-indicator --------------- */
 let mainInfoToggler = document.getElementsByClassName("main-info__toggler");
 
@@ -121,7 +125,7 @@ function mainInfoIndicatorToggler1() {
   }
 }
 
-mainInfoToggler[0].addEventListener("click", mainInfoIndicatorToggler1)
+/* mainInfoToggler[0].addEventListener("click", mainInfoIndicatorToggler1);  */
 
 function mainInfoIndicatorToggler2() {
   if (this.childNodes[1].childNodes[1].className.match(/(?:^|\s)fa-chevron-up(?!\S)/)) {
@@ -134,12 +138,11 @@ function mainInfoIndicatorToggler2() {
 for (let i = 1; i < mainInfoToggler.length; i++) {
   mainInfoToggler[i].addEventListener("click", mainInfoIndicatorToggler2)
 }
-
 /* --------------- hết .main-info__toggler-indicator --------------- */
 
 
-/* --------------- sample image toggler --------------- */
 
+/* --------------- sample image toggler --------------- */
 let sampleImgModal = document.querySelector("#sample--full-size img");
 let imgToggler = document.querySelectorAll("#sample img");
 
@@ -161,9 +164,131 @@ let mainImgModal = document.querySelector("#main-img--full-size img");
   mainImgModal.src = mainImg.style.backgroundImage
 } */
 
-console.log(mainImg.style.backgroundImage);
+/* console.log(mainImg.style.backgroundImage); */
 
 /* --------------- hết sample image toggler --------------- */
+
+
+
+/* --------------- quantity calc --------------- */
+
+/* ---------- grand final calc ---------- */
+function grandFinalCalc() {
+  let grandTotal = document.getElementsByClassName("cart__grand-total")[0];
+
+  let grandTotalNum = 0;
+  
+  let allMultiple = document.getElementsByClassName("col--multiple-price");
+  
+  for (let i = 1; i < allMultiple.length; i++) {
+    let eachMultiple = +allMultiple[i].innerHTML.slice(12, allMultiple[i].innerHTML.length - 1).replace(/\./g, "");
+    grandTotalNum += eachMultiple;
+  }
+  
+  grandTotal.innerHTML = `Tổng giá trị giỏ hàng: ${grandTotalNum.toLocaleString()}đ`
+}
+
+grandFinalCalc();
+
+
+/* ---------- add calc ---------- */
+let addQuantityBtn = document.getElementsByClassName("quantity__plus");
+
+for (let i = 0; i < addQuantityBtn.length; i++) {
+  addQuantityBtn[i].addEventListener("click", function() {
+    let currentNum = +this.previousElementSibling.innerHTML;
+    
+    this.previousElementSibling.innerHTML = currentNum + 1;
+
+    let singlePrice = this.parentElement.previousElementSibling.innerHTML;
+
+    let singlePriceNum = +singlePrice.slice(9, singlePrice.length - 1).replace(/\./g, "");
+
+    let multiplePriceValue = +this.previousElementSibling.innerHTML * singlePriceNum;
+
+    this.parentElement.nextElementSibling.innerHTML = `Thành tiền: ${multiplePriceValue.toLocaleString()}đ`
+  });
+
+  addQuantityBtn[i].addEventListener("click", grandFinalCalc);
+}
+
+
+/* ---------- minus calc ---------- */
+let minusQuantityBtn = document.getElementsByClassName("quantity__minus");
+
+for (let i = 0; i < minusQuantityBtn.length; i++) {
+  minusQuantityBtn[i].addEventListener("click", function() {
+    let currentNum = +this.nextElementSibling.innerHTML;
+
+    if (currentNum > 1) {
+      this.nextElementSibling.innerHTML = currentNum - 1;
+    }
+
+    let singlePrice = this.parentElement.previousElementSibling.innerHTML;
+
+    let singlePriceNum = +singlePrice.slice(9, singlePrice.length - 1).replace(/\./g, "");
+
+    let multiplePriceValue = +this.nextElementSibling.innerHTML * singlePriceNum;
+
+    this.parentElement.nextElementSibling.innerHTML = `Thành tiền: ${multiplePriceValue.toLocaleString()}đ`
+  });
+
+  minusQuantityBtn[i].addEventListener("click", grandFinalCalc);
+}
+
+/* --------------- hết quantity calc --------------- */
+
+
+
+/* --------------- remove cart item --------------- */
+
+/* ---------- check empty cart ---------- */
+function checkEmptyCart() {
+  let checkEmptyGrandTotal = document.getElementsByClassName("cart__grand-total")[0].innerHTML
+  if (checkEmptyGrandTotal == "Tổng giá trị giỏ hàng: 0đ") {
+    document.getElementsByClassName("cart__grand-total")[0].innerHTML = "Giỏ hàng trống"
+  }
+}
+
+/* ---------- remove item ---------- */
+let removeItemBtn = document.querySelectorAll(".col--remove.display-till-md");
+
+for (let i = 0; i < removeItemBtn.length; i++) {
+  removeItemBtn[i].addEventListener("click", function() {
+    this.parentElement.parentElement.remove();
+    let grandTotal = document.getElementsByClassName("cart__grand-total")[0];
+
+    let grandTotalNum = 0;
+
+    let allMultiple = document.getElementsByClassName("col--multiple-price");
+
+    for (let i = 1; i < allMultiple.length; i++) {
+      let eachMultiple = +allMultiple[i].innerHTML.slice(12, allMultiple[i].innerHTML.length - 1).replace(/\./g, "");
+      grandTotalNum += eachMultiple;
+    }
+
+    grandTotal.innerHTML = `Tổng giá trị giỏ hàng: ${grandTotalNum.toLocaleString()}đ`
+  });
+
+  removeItemBtn[i].addEventListener("click", checkEmptyCart);
+}
+
+/* --------------- hết remove cart item --------------- */
+
+
+
+
+/* --------------- hết quantity calc --------------- */
+
+
+
+
+
+
+
+
+
+
 
 
 
