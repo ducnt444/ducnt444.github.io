1fee416acfc97b6a8c46714f276762615193407d
ducnt <ducnt444@gmail.com>
2021-03-07 12:19:41 +0700

    wf

--- halo/js/script.js
+++ halo/js/script.js
@@ -109,24 +109,32 @@ function logregSwitch() {
 
 /* 
 ------------------------- 
-------------------------- search result 
+------------------------- scroll top
 ------------------------- 
 */
-/* let searchInput = document.querySelector(".search-area-full");
-let searchResult = document.querySelector(".search__result");
-let searchBtn = document.querySelectorAll(".search-btn");
-searchResult.innerText = searchInput.value;
 
-if (searchResult) {
-  for (let i = 0; i < searchBtn.length; i+=2) {
-    searchBtn[i].addEventListener("click", function(){
-      searchResult.innerText = searchInput.value;
-    })
+//Get the button:
+scrollTopBtn = document.querySelector(".scroll-up-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 575 || document.documentElement.scrollTop > 575) {
+    scrollTopBtn.style.display = "flex";
+  } else {
+    scrollTopBtn.style.display = "none";
   }
-} */
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollTopBtn.addEventListener("click", function(){
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE, Opera
+})
 
 /* 
-------------------------- hết search result 
+------------------------- hết scroll top
 ------------------------- 
 */
 
