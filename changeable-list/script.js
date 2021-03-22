00791ac45f257e09a2b207df0a95e4ae5b2c7f06
ducnt <ducnt444@gmail.com>
2021-03-22 10:49:48 +0700

    f

--- changeable-list/script.js
+++ changeable-list/script.js
@@ -1,31 +1,52 @@
 
 function loadDoc() {
   //tạo
-  var xhttp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
   //code sẽ chạy khi lấy được dữ liệu từ server
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let users = JSON.parse(this.responseText); 
-      let content = ""
+      let content = "";
       for (let i = 0; i < users.length; i++) {
-        content += `<tr>
+        content += 
+        `<tr>
           <td>${users[i].name}</td>
-          <td>${users[i].birthday}</td>
+          <td>${users[i].birthday.substr(-4)}</td>
           <td>${users[i].email}</td>
           <td>${users[i].phone}</td>
-          <td><a class="btn btn--edit text-primary" href="details.html"> <span><i class="far fa-edit"></i></span><span class="ml-2">Chỉnh sửa</span></a><span>|</span>
-            <button class="btn btn--delete text-danger"> <span><i class="fas fa-trash-alt"></i></span><span class="ml-2">Xóa</span></button></td>
-
+          <td>
+          <a class="btn btn--edit text-primary" href="details.html">
+            <span> <i class="far fa-edit"></i> </span>
+            <span class="ml-2">Chỉnh sửa</span>
+          </a>
+          <span>|</span>
+          <button class="btn btn--delete text-danger">
+            <span> <i class="fas fa-trash-alt"></i> </span>
+            <span class="ml-2">Xóa</span>
+          </button>
+          </td>
         </tr>`;
-        console.log(users[i].name);
       }
       $("tbody").html(content);
-      console.log(content);
     }
   };
+  
   //GET từ server
   xhttp.open("GET", "http://localhost:3000/users", true);
   xhttp.send();
 }
 
 loadDoc();
+
+$("#save").click(() => {
+  $.post(
+    "http://localhost:3000/users",
+    {
+      "name": $("input")[0].value,
+      "birthday": $("input")[1].value,
+      "email": $("input")[2].value,
+      "phone": $("input")[3].value
+    }
+  );
+});
+
