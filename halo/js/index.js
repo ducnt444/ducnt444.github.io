c48dab7eab83ef2a26cc37a5cbbac6cafee1a17b
ducnt <ducnt444@gmail.com>
2021-03-23 17:26:31 +0700

    op

--- halo/js/index.js
+++ halo/js/index.js
@@ -5,3 +5,33 @@ $(window).on('load', function() {
   }
   sessionStorage.setItem("updates", "off");
 });
+
+
+/* 
+-----------------------------------
+ loading screen
+-----------------------------------
+*/
+//counter
+
+let loading =  document.getElementById("loading");
+let counter =  document.getElementById("counter__num");
+
+let loadingCounter = setInterval(() => {
+  counter.innerHTML = parseInt(counter.innerHTML) + 1
+  }, 30)
+
+setTimeout(() => {
+  clearTimeout(loadingCounter);
+  loading.style.visibility = "hidden";
+  loading.style.opacity = "0";
+  if (sessionStorage.getItem("loading") == null) {
+    sessionStorage.setItem("loading", "done")
+  }
+}, 3000)
+
+/* 
+-----------------------------------
+ hết loading screen
+-----------------------------------
+*/
--- halo/js/script.js
+++ halo/js/script.js
@@ -58,35 +58,6 @@ logRegPortal.click(() => {
 
 
 
-/* 
------------------------------------
- loading screen
------------------------------------
-*/
-//counter
-
-let loading =  document.getElementById("loading");
-let counter =  document.getElementById("counter__num");
-
-let loadingCounter = setInterval(() => {
-  counter.innerHTML = parseInt(counter.innerHTML) + 1
-  }, 30)
-
-setTimeout(() => {
-  clearTimeout(loadingCounter);
-  loading.style.visibility = "hidden";
-  loading.style.opacity = "0";
-  if (sessionStorage.getItem("loading") == null) {
-    sessionStorage.setItem("loading", "done")
-  }
-}, 3000)
-
-/* 
------------------------------------
- hết loading screen
------------------------------------
-*/
-
 
 
 /* 
