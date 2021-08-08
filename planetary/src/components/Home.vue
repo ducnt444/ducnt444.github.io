27d0321911de4ec0fe5b18113cc585c377faf1f6
ducnt <ducnt444@gmail.com>
2021-08-08 08:14:06 +0700

    vue

---
+++ planetary/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="homework-area">
+    {{content}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Home",
+  data: function () {
+    return {
+      content: "VUE!"
+    };
+  },
+});
+</script>
